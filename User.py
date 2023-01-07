6c9e02c64dd8597ec41d8167082fcb8a49e23646
Hammad Ahmed <62638943+HunterTravis@users.noreply.github.com>
2023-01-07 19:23:14 +0500

    Final

--- User.py
+++ User.py
@@ -4,14 +4,5 @@ class User:
         self.nationality = nationality
         self.dob = dob
 
-    def getID(self):
-        return self.id
-
-    def getName(self):
-        return self.name
-
-    def getAge(self):
-        return self.age
-
     def __str__(self):
         return f"Name: {self.name} | Nationality: {self.nationality}\t | DOB: {self.dob}"
