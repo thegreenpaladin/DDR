6e6935fb069ed452fa267f6c9c66dfcc38cbcb84
Fatima-jamal <fjamalnet@gmail.com>
2023-01-07 20:16:07 +0500

    seperate func for mem usage, err handle in db qry

--- LazyLoadingPattern.py
+++ LazyLoadingPattern.py
@@ -4,21 +4,25 @@ import os
 import psutil
 from RecordListProxy import RecordListProxy
 
+
+def getCurrentProcessMemoryUsage():
+    return round(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2, 2)
+
+
 recordList = RecordListProxy()
 
 # memory used by program before the objects are created
-print("\nMemory used before loading the objects:", round(
-    psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2, 2), "MiBs")
+print("\nMemory used before loading the objects:",getCurrentProcessMemoryUsage() , "MiBs")
 
 print('\nFetching user list...\n')
 # this will load the records from the database as list of objects
 usrList = recordList.getUserList()
 
 # showing memory used by program after the objects are created
-print("Memory used after loading the objects:", round(
-    psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2, 2), "MiBs\n")
+print("Memory used after loading the objects:", getCurrentProcessMemoryUsage(), "MiBs\n")
 
 # printing 6 objects from the list
 print("Below are 6 records from the database:\n")
 for x in range(6):
     print(usrList[x])
+
--- RecordListImplemented.py
+++ RecordListImplemented.py
@@ -4,13 +4,15 @@ import mysql.connector
 
 class RecordListImplemented(RecordList):
     def __init__(self) -> None:
-        self.mydb = mysql.connector.connect(
-            host="localhost", user="root", password="", database="ddrproject")
-        self.mycursor = self.mydb.cursor()
         try:
+            self.mydb = mysql.connector.connect(
+                host="localhost", user="root", password="", database="ddrproject")
+            self.mycursor = self.mydb.cursor()
+        
             self.mycursor.execute("SELECT * FROM records")
         except:
-            print('The connection to the Database Server could not be established.\n Please make sure your Database Server is on, and run the "RecordAdder.py" file again. Database will automatically be created.')
+            print('The connection to the Database Server could not be established.\nPlease make sure your Database Server is on, and run the "RecordAdder.py" file again. Database will automatically be created.')
+            exit()
 
     def getUserList(self):
         self.usrList = []
